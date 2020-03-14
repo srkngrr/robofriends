655ac904325f14666b42e67a8bc3ea39f9501990
Serkan <serkangurer@yahoo.com>
2020-03-14 16:52:02 +0300

    Updates

---
+++ precache-manifest.33cac4f8986dcd89829ba5bd9f2b69bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b486e49a2d10ec2bad8f4baaac3e84",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "480ac218011191fb3a2b",
+    "url": "/robofriends/static/css/main.659ebc25.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480ac218011191fb3a2b",
+    "url": "/robofriends/static/js/main.c8b570d8.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
